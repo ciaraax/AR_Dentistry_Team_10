c522120e943322f200a89f5f0fbc6a7a10290e7c
Ciara Langton <ciara8264@googlemail.com>
2019-03-12 10:31:48 +0000

    Created a small pop up app

---
+++ AR_Team10_Project/Assets/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using System.Collections.Generic;
+
+public class InputHandler : MonoBehaviour 
+{
+	
+
+	void Update () 
+	{
+	}
+
+	void Start(){
+	}
+}
---
+++ AR_Team10_Project/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+public class NewBehaviourScript1 : MonoBehaviour {
+	
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+
+	} 
+}
