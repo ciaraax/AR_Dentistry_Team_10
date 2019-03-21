2e46dcb9416a84d35741968da36b6db9f00ff8c6
Masroor Unar <UnarM@cardiff.ac.uk>
2019-03-21 17:12:26 +0000

    Added a virtual button and created a c# script

---
+++ ArDrawing/Assets/vb.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb : MonoBehaviour{
+
+	void OnMouseDown()
+	{
+		ImageTargetType.PREDEFINED.Equals("ImageCorrect");
+	}
+}
