cd57690edfcdef39e120e40a89715ff0ba7c914f
Ishaq Hussain <HussainI6@cardiff.ac.uk>
2019-04-02 00:30:43 +0100

    made a 2d game following tutorial

---
+++ ArDrawing/Assets/DisableVuforia.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DisableVuforia : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		VuforiaBehaviour.Instance.enabled = false;
+		//MainCamera.Vuforia.VuofriaBehaviour.enable (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OffVuforia()
+	{
+		VuforiaBehaviour.Instance.enabled = false;
+	}
+}
