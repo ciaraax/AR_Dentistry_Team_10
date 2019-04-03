5f2f392725c5fc3d19c5dc7c675996167e8cfd51
Masroor Unar <UnarM@cardiff.ac.uk>
2019-04-03 02:13:37 +0100

    Added Virtual button with on click animation using C# script

--- ArDrawing/Assets/vb.cs
+++ ArDrawing/Assets/vb.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class vb : MonoBehaviour{
+public class vb : MonoBehaviour, IVirtualButtonEventHandler{
 
-	void OnMouseDown()
-	{
-		ImageTargetType.PREDEFINED.Equals("ImageCorrect");
+	public GameObject vbBtnObj;
+	public Animator teethAnimation;
+
+
+	void Start () {
+
+		vbBtnObj = GameObject.Find("VirtualButton");
+		vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		teethAnimation.GetComponent<Animator> ();
+
 	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb) 
+	{
+		teethAnimation.Play("teeth_animation");
+		Debug.Log ("Button Pressed");
+
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb) 
+	{
+		teethAnimation.Play("none");
+		Debug.Log ("Button Released");
+
+	}
+
 }
