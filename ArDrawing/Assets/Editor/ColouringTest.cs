996d2996571a8ea8eb0f244912a7ebe50ef96014
Ciara Langton <ciara8264@googlemail.com>
2019-04-04 13:03:09 +0100

    Added colouring tests

---
+++ ArDrawing/Assets/Editor/ColouringTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ColouringTest {
+
+	private RegionCapture rc;
+	private RenderTextureCamera rtc;
+
+	[SetUp]
+	public void SetUp() {
+
+		rc = new RegionCapture ();
+		rtc = new RenderTextureCamera ();
+
+	}
+
+	[Test]
+	public void ColouringTestSimplePasses() {
+
+		Assert.IsFalse(rc.FlipX);
+		Assert.IsFalse(rc.FlipY);
+	}
+
+	[Test]
+	public void textureResolution() {
+
+		Assert.AreEqual(512, rtc.TextureResolution);
+
+
+	}
+
+
+}
