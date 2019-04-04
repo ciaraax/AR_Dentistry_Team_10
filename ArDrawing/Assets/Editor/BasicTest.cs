d59facca4ec38508935cdc28985cd3243c5e2340
Masroor Unar <UnarM@cardiff.ac.uk>
2019-04-04 10:44:40 +0100

    added basic tests

---
+++ ArDrawing/Assets/Editor/BasicTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class BasicTest {
+
+	[Test]
+	public void Basic()
+	{
+
+		bool isActive = false;
+
+		Assert.AreEqual(false, isActive);
+
+	}
+
+	[Test]
+	public void CatchingErrors()
+	{
+
+		GameObject gameObject = new GameObject("test");
+
+		Assert.Throws<MissingComponentException>(
+			() => gameObject.GetComponent<Rigidbody>().velocity = Vector3.one
+		);
+
+	}
+
+
+
+}
